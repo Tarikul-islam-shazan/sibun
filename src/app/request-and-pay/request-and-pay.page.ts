9a8e46b988a933f676e8c09980b4b627832b2949
KM TARIKUL ISLAM <tarikulislam@users-MacBook-Pro.local>
2021-02-11 05:19:58 +0600

    Request and pay

--- src/app/request-and-pay/request-and-pay.page.ts
+++ src/app/request-and-pay/request-and-pay.page.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestAndPayPage implements OnInit {
 
+  amount:string = '0.00';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateAmount(num: string){
+    if(num === 'x'){
+        this.amount =  this.amount.substr(0,this.amount.length-1); 
+    }  else{
+      this.amount = this.amount == '0.00' ?   num : (this.amount + '' + num ) ;
+    }
+    
+    
   }
 
 }
