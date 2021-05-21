31fb389883d3c5fd64acd75d6890567fdba1dcd6
KM TARIKUL ISLAM <tarikulislam@users-MacBook-Pro.local>
2021-05-21 13:07:14 +0600

    update service url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serviceUrl: 'http://3.137.137.88:3000/',
+  serviceUrl: 'http://18.117.79.23:3000/',
   plaid: {
     sandbox: '34565371649e50ff68fee9b85e04a4'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serviceUrl: 'http://3.137.137.88:3000/',
+  serviceUrl: 'http://18.117.79.23:3000/',
   localServiceUrl: 'http://localhost:5000/',
   plaid : {
     sandbox: '34565371649e50ff68fee9b85e04a4'
